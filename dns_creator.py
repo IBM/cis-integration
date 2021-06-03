776d66ad1f5c857a1903a356b484df4833efdddd
Lucas-Franke <Lucas.Franke@ibm.com>
2021-06-03 16:16:19 -0400

    additionally creates a www dns record to redirect the www subdomain to the app

--- dns_creator.py
+++ dns_creator.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from ibm_cloud_networking_services import DnsRecordsV1
 
 def main():
-    load_dotenv()
+    env_path = input("Enter the path for your .env file: ")
+    load_dotenv(env_path)
     # read crn, zone id and end-point from environment
     crn = os.getenv("CRN")
     zone_id = os.getenv("ZONE_ID")
@@ -13,11 +14,17 @@ def main():
         crn=crn, zone_identifier=zone_id, service_name="cis_services")
     record.set_service_url(endpoint)
     record_type = 'CNAME'
-    name = '@'
+    name = '@' # creating a root DNS record
     content = os.getenv("APP_URL") # For now an env variable, could be user input
-    resp = record.create_dns_record(
+    resp1 = record.create_dns_record(
         type=record_type, name=name, content=content)
-    print(resp)
+    print(resp1)
+
+    # creating a DNS record for the www subdomain
+    name = 'www'
+    resp2 = record.create_dns_record(
+        type=record_type, name=name, content=content)
+    print(resp2)
 
 if __name__ == "__main__":
     main()
