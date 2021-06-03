09e54d0d60122f2d8cc118e1fcb5a5bb6725cd39
Akul Joshi <joshi.akul@ibm.com>
2021-06-03 11:39:34 -0500

    Connected the load balancer

--- create-glb.py
+++ create-glb.py
@@ -13,7 +13,7 @@ def main():
     endpoint = os.getenv("API_ENDPOINT")
 
     name = "test-lb-pool1"
-    origins = [{"name": "app-server-1", "address": "www.gcat-interns-rock.com", "enabled": True, "weight":1}]
+    origins = [{"name": "app-server-1", "address": "alpha1.example.com", "enabled": True, "weight":1}]
 
     monitor = GlobalLoadBalancerMonitorV1.new_instance(crn=crn, service_name="cis_services")
     health_check = monitor.create_load_balancer_monitor(description="test-monitor", crn=crn, type="https", expected_codes="2xx", follow_redirects=True).get_result()
@@ -21,8 +21,15 @@ def main():
     print("Monitor ID:", monitor_id)
 
     origin_pools = GlobalLoadBalancerPoolsV0.new_instance(crn=crn, service_name="cis_services")
-    resp = origin_pools.create_load_balancer_pool(name=name, origins=origins, enabled=True, monitor=monitor_id).get_result()
-    print("Origin Pools Response:", resp)
+    origin_pool_result = origin_pools.create_load_balancer_pool(name=name, origins=origins, enabled=True, monitor=monitor_id).get_result()
+    origin_pool_id = origin_pool_result["result"]["id"]
+    print("Origin Pool ID:", origin_pool_id)
+
+    global_load_balancer = GlobalLoadBalancerV1.new_instance(crn=crn, zone_identifier=zone_identifier, service_name="cis_services")
+    global_load_balancer.set_service_url(endpoint)
+    global_load_balancer_result = global_load_balancer.create_load_balancer(name="alpha1.example.com", default_pools=[origin_pool_id], fallback_pool=origin_pool_id, enabled=True, proxied=True).get_result()
+    global_load_balancer_id = global_load_balancer_result["result"]["id"]
+    print("Global Load Balancer ID:", global_load_balancer_id)
 
 if __name__ == "__main__":
     main()
