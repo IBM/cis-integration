d162b01e2b892222814bcc4336606ca35b8e9fb9
Christopher Springstead <Christopher.Springstead@ibm.com>
2021-06-03 12:41:18 -0500

    origin pool name check
    might need to change based on how user input is done

--- create-glb.py
+++ create-glb.py
@@ -21,15 +21,19 @@ def main():
     print("Monitor ID:", monitor_id)
 
     origin_pools = GlobalLoadBalancerPoolsV0.new_instance(crn=crn, service_name="cis_services")
-    origin_pool_result = origin_pools.create_load_balancer_pool(name=name, origins=origins, enabled=True, monitor=monitor_id).get_result()
-    origin_pool_id = origin_pool_result["result"]["id"]
-    print("Origin Pool ID:", origin_pool_id)
+    origin_pool_name_check_resp = origin_pools.list_all_load_balancer_pools().get_result()
+    if name != origin_pool_name_check_resp["result"]["name"]:
+        origin_pool_result = origin_pools.create_load_balancer_pool(name=name, origins=origins, enabled=True, monitor=monitor_id).get_result()
+        origin_pool_id = origin_pool_result["result"]["id"]
+        print("Origin Pool ID:", origin_pool_id)
+    else:
+        print("A origin pool with taht name already exists.")
 
     global_load_balancer = GlobalLoadBalancerV1.new_instance(crn=crn, zone_identifier=zone_identifier, service_name="cis_services")
     global_load_balancer.set_service_url(endpoint)
-    name_check_resp = global_load_balancer.list_all_load_balancers().get_result()
-    print(name_check_resp)
-    if name != name_check_resp["result"]["name"]:
+    glb_name_check_resp = global_load_balancer.list_all_load_balancers().get_result()
+
+    if name != glb_name_check_resp["result"]["name"]:
         global_load_balancer_result = global_load_balancer.create_load_balancer(name="alpha1.example.com", default_pools=[origin_pool_id], fallback_pool=origin_pool_id, enabled=True, proxied=True).get_result()
         global_load_balancer_id = global_load_balancer_result["result"]["id"]
         print("Global Load Balancer ID:", global_load_balancer_id)
