b77a6e3eb7e2705f28af4e375d1d48b1256bfefe
Christopher Springstead <Christopher.Springstead@ibm.com>
2021-06-04 09:25:45 -0500

    update glb and origin pool structure

--- create-glb.py
+++ create-glb.py
@@ -23,15 +23,17 @@ def main():
 
     origin_pools = GlobalLoadBalancerPoolsV0.new_instance(crn=crn, service_name="cis_services")
     origin_pool_name_check_resp = origin_pools.list_all_load_balancer_pools().get_result()["result"]
-    origin_pool_names = [origin_pool_name_check_resp[i]["name"] for i in range(len(origin_pool_name_check_resp))]
-    origin_pool_ids = [origin_pool_name_check_resp[i]["id"] for i in range(len(origin_pool_name_check_resp))]
+    
+    origin_pools_dict = {}
+    for i in range(len(origin_pool_name_check_resp)):
+        origin_pools_dict[origin_pool_name_check_resp[i]["name"]] = origin_pool_name_check_resp[i]["id"]
 
-    if name not in origin_pool_names:
+    if name not in origin_pools_dict:
         origin_pool_result = origin_pools.create_load_balancer_pool(name=name, origins=origins, enabled=True, monitor=monitor_id).get_result()
         origin_pool_id = origin_pool_result["result"]["id"]
     else:
         print("A origin pool with that name already exists.")
-        origin_pool_id = origin_pool_ids[origin_pool_names.index(name)]
+        origin_pool_id = origin_pools_dict[name]
         origin_pools.edit_load_balancer_pool(origin_pool_id, name=name, origins=origins, enabled=True, monitor=monitor_id)
 
     print("Origin Pool ID:", origin_pool_id)
@@ -41,15 +43,17 @@ def main():
     global_load_balancer = GlobalLoadBalancerV1.new_instance(crn=crn, zone_identifier=zone_identifier, service_name="cis_services")
     global_load_balancer.set_service_url(endpoint)
     glb_name_check_resp = global_load_balancer.list_all_load_balancers().get_result()["result"]
-    glb_names = [glb_name_check_resp[i]["name"] for i in range(len(glb_name_check_resp))]
-    glb_ids = [glb_name_check_resp[i]["id"] for i in range(len(glb_name_check_resp))]
 
-    if load_bal_name not in glb_names:
+    glb_dict = {}
+    for i in range(len(glb_name_check_resp)):
+        glb_dict[glb_name_check_resp[i]["name"]] = glb_name_check_resp[i]["id"]
+
+    if load_bal_name not in glb_dict:
         global_load_balancer_result = global_load_balancer.create_load_balancer(name=load_bal_name, default_pools=[origin_pool_id], fallback_pool=origin_pool_id, enabled=True, proxied=True).get_result()
         global_load_balancer_id = global_load_balancer_result["result"]["id"]
     else:
         print("A global load balancer with this name already exists.")
-        global_load_balancer_id = glb_ids[glb_names.index(load_bal_name)]
+        global_load_balancer_id = glb_dict[name]
         global_load_balancer.edit_load_balancer(global_load_balancer_id, name=load_bal_name, default_pools=[origin_pool_id], fallback_pool=origin_pool_id, enabled=True, proxied=True)
 
     print("Global Load Balancer ID:", global_load_balancer_id)
