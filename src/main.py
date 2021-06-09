53fba5d0479459abac07c3fd3e34c3255f4c0296
Christopher Springstead <Christopher.Springstead@ibm.com>
2021-06-09 13:38:28 -0500

    added create_envfile which takes user info and creates needed .env file

--- src/main.py
+++ src/main.py
@@ -24,6 +24,14 @@ class IntegrationInfo:
     app_url = ''
     cis_api_key = ''
 
+    # used to create .env file
+    def create_envfile(self):
+        info = ["CRN="+"\""+self.crn+"\""+"\n", "ZONE_ID="+"\""+self.zone_id+"\""+"\n", "API_ENDPOIT="+"\""+self.api_endpoint+"\""+"\n", "CIS_SERVICES_APIKEY="+"\""+self.cis_api_key+"\""+"\n"]
+        file = open("credentials.env", "w")
+        file.writelines(info)
+        file.close()
+
+
 def print_help():
     print(Color.BOLD + 'NAME:' + Color.END)
     print("\tcis-integration - a command line tool used to connect a CIS instance with an application deployed on Code Engine\n")
@@ -35,28 +43,29 @@ def print_help():
     print("\t--help, -h \t\t show help\n")
 
 def main():
-    
+    UserInfo = IntegrationInfo()
+
     if len(sys.argv) > 1 and sys.argv[1] == '-h':
         print_help()
         sys.exit(1)
 
     try:
-        IntegrationInfo.crn = sys.argv[1]
+        UserInfo.crn = sys.argv[1]
     except IndexError:
         print("You did not specify a CIS CRN.")
         sys.exit(1)
 
     try:
-        IntegrationInfo.zone_id = sys.argv[2]
+        UserInfo.zone_id = sys.argv[2]
     except IndexError:
         print("You did not specify a CIS Zone_ID.")
         sys.exit(1)
     
     try:
-        IntegrationInfo.app_url = sys.argv[3]
+        UserInfo.app_url = sys.argv[3]
     except IndexError:
         print("You did not specify a application URL.")
         sys.exit(1)
 
-    IntegrationInfo.cis_api_key = getpass.getpass(prompt="Enter CIS Services API Key: ")
-    
+    UserInfo.cis_api_key = getpass.getpass(prompt="Enter CIS Services API Key: ")
+    UserInfo.create_envfile()
