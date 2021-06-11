a3ccc80ebfe5726629b9a342b159b87ca5bb5e14
Lucas-Franke <Lucas.Franke@ibm.com>
2021-06-11 11:59:59 -0400

    No longer uses bash script to request the refresh token. Uses the Requests library instead

--- create_terraform_workspace.py
+++ create_terraform_workspace.py
@@ -1,25 +1,21 @@
 
 from ibm_schematics.schematics_v1 import SchematicsV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
-import time
-import subprocess
+import os, requests, time
 from dotenv import load_dotenv
 from ibm_cloud_sdk_core import ApiException
 
 def main():
-    load_dotenv()
+    load_dotenv("./lucas_env.env")
     apikey = os.getenv("CIS_SERVICES_APIKEY")
     schematics_url = os.getenv("SCHEMATICS_URL")
     github_PAT = os.getenv("GITHUB_PAT")
     authenticator = IAMAuthenticator(apikey)
     schematics_service = SchematicsV1(authenticator = authenticator)
     schematics_service.set_service_url(schematics_url)
+    r_token = request_token(apikey)
 
-    r_token = subprocess.Popen("./refresh_token.sh", shell=True, stdout=subprocess.PIPE)
-    last_token = r_token.stdout.read()
-    encoding = 'utf-8'
-    last_token = last_token.decode(encoding).replace('\n','')
+    # Setting up the necessary information to create the workspace
     workspace_variable_request_model = {}
     workspace_variable_request_model['name'] = 'ibmcloud_api_key'
     workspace_variable_request_model['value'] = apikey
@@ -33,7 +29,7 @@ def main():
     template_repo_request_model = {}
     template_repo_request_model['url'] = 'https://github.ibm.com/GCAT/cis-integration'
 
-
+    # Creating the workspace and connecting to the github repo
     workspace_response = schematics_service.create_workspace(
         description="Testing creating a workspace using a python script", 
         name="test-code-workspace-1",
@@ -48,31 +44,39 @@ def main():
     print('Successfully created the workspace!')
     workspace_activity_plan_result = None
     keepgoing = True
+
+    # Generate a plan from the imported terraform files
     while keepgoing:
+        # The command will throw an ApiException while the workspace is still generating,
+        # so we catch that exception and try again every 2 seconds as long as some other
+        # error does not occur
         try:
             workspace_activity_plan_result = schematics_service.plan_workspace_command(
                 w_id=workspace_response["id"],
-                refresh_token=last_token
+                refresh_token=r_token
                 
             ).get_result()
             break
         except ApiException as ae:
-
+            # Error 409 means that the workspace is still busy, we just have to wait for it to finish
             if ae.http_response.status_code == 409:
                 print('Generating a plan....')
                 time.sleep(2)
             else:
+                # Some other error occurred so we need to break out of the loop and end execution
                 print("Error {0}: ".format(ae.http_response.status_code) + ae.message)
                 keepgoing = False
                 break
-    
+    # Now we apply the plan if it was successfully generated
     if keepgoing:
         print('Plan Generated! Applying:')
+        # This command will also throw an ApiException while the plan is still finalizing,
+        # so we just wait for it to finish
         while keepgoing:
             try:
                 workspace_activity_apply_result = schematics_service.apply_workspace_command(
                     w_id=workspace_response["id"],
-                    refresh_token=last_token
+                    refresh_token=r_token
                     
                 ).get_result()
                 break
@@ -88,7 +92,22 @@ def main():
         print('Resources built successfully!')
 
     
+def request_token(apikey: str):
+    """
+    Requests a refresh token for the client so that we can execute the plan and apply commands in
+    the workspace.
     
+    param: apikey is the client's IBM Cloud Apikey
+
+    returns: the generated refresh token
+    """
+    data={'grant_type': 'urn:ibm:params:oauth:grant-type:apikey', 'apikey': apikey}
+    headers= {'Accept': 'application/json',
+            'Content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic Yng6Yng='}
+    url="https://iam.cloud.ibm.com/identity/token"
+    token = requests.post(url=url, data=data, headers=headers)
+    return token.json()["refresh_token"] 
     
 
 if __name__ == "__main__":
