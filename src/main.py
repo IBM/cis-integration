fefdabfa14c81cf6df26277f69319195b9e6b828
Christopher Springstead <Christopher.Springstead@ibm.com>
2021-06-14 15:27:13 -0500

    update to add CIS_DOMAIN to .env

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ class IntegrationInfo:
     api_endpoint = 'https://api.cis.cloud.ibm.com'
     app_url = ''
     cis_api_key = ''
-    hostname = ''
+    cis_domain = ''
     schematics_url = 'https://us.schematics.cloud.ibm.com'
     github_pat = ''     # might need to be removed later
 
@@ -24,8 +24,9 @@ class IntegrationInfo:
         os.environ["ZONE_ID"] = self.zone_id
         os.environ["API_ENDPOINT"] = self.api_endpoint
         os.environ["CIS_SERVICES_APIKEY"] = self.cis_api_key
+        os.environ["CIS_DOMAIN"] = self.cis_domain
 
-        info = ["CRN=\""+self.crn+"\"\n", "ZONE_ID=\""+self.zone_id+"\"\n", "API_ENDPOINT=\""+self.api_endpoint+"\"\n", "CIS_SERVICES_APIKEY=\""+self.cis_api_key+"\"\n", "SCHEMATICS_URL=\""+self.schematics_url+"\"\n", "GITHUB_PAT=\""+self.github_pat+"\"\n"]
+        info = ["CRN=\""+self.crn+"\"\n", "ZONE_ID=\""+self.zone_id+"\"\n", "API_ENDPOINT=\""+self.api_endpoint+"\"\n", "CIS_SERVICES_APIKEY=\""+self.cis_api_key+"\"\n", "CIS_DOMAIN=\""+self.cis_domain+"\"\n", "SCHEMATICS_URL=\""+self.schematics_url+"\"\n", "GITHUB_PAT=\""+self.github_pat+"\"\n"]
         file = open("credentials.env", "w")
         file.writelines(info)
         file.close()
@@ -35,7 +36,7 @@ def print_help():
     print("\tcis-integration - a command line tool used to connect a CIS instance with an application deployed on Code Engine\n")
 
     print(Color.BOLD + "USAGE:" + Color.END)
-    print("\t[enviornment variables] cis-integration [global options] [CIS CRN] [CIS ID] [CIS HOSTNAME] [CODE ENGINE APP URL] [GITHUB PAT]\n")
+    print("\t[enviornment variables] cis-integration [global options] [CIS CRN] [CIS ID] [CIS Domain] [CODE ENGINE APP URL] [GITHUB PAT]\n")
 
     print(Color.BOLD + "GLOBAL OPTIONS:" + Color.END)
     print("\t--help, -h \t\t show help\n")
@@ -60,9 +61,9 @@ def main():
         sys.exit(1)
     
     try:
-        UserInfo.hostname = sys.argv[3]
+        UserInfo.cis_domain = sys.argv[3]
     except IndexError:
-        print("You did not specify a CIS hostname.")
+        print("You did not specify a CIS Domain.")
         sys.exit(1)
 
     try:
@@ -87,7 +88,7 @@ def main():
     
     # 2. Global Load Balancer
     user_GLB = GLB()
-    user_GLB.create_glb(UserInfo.hostname)
+    user_GLB.create_glb(UserInfo.cis_domain)
 
     # 3. TLS Certificate Configuration
     certcreate.main()
