812d98a337cc03b8a87c682f35a51fcc36b5fba0
Akul Joshi <joshi.akul@ibm.com>
2021-06-17 14:06:02 -0500

    added comments to improve readibility

--- src/main.py
+++ src/main.py
@@ -53,6 +53,7 @@ class IntegrationInfo:
         file.writelines(info)
         file.close()
 
+    # loads .env file if it exists
     def remove_quotes(self, item):
         result = item
         if result[0] == '"':
@@ -70,31 +71,36 @@ class IntegrationInfo:
 
         load_dotenv(filename)          
 
+# method used to display the command usage if user uses `-h` or `--help`
 def print_help():
     print(Color.BOLD + 'NAME:' + Color.END)
     print("\tcis-integration - a command line tool used to connect a CIS instance with an application deployed on Code Engine\n")
 
     print(Color.BOLD + "USAGE:" + Color.END)
     print("\t[python implementation]\t\tcis-integration [global options] [CIS CRN] [CIS ID] [CIS DOMAIN] [CODE ENGINE APP URL] [GITHUB PAT]")
-    print("\t[terraform implementation]\tcis-integration [global options] [RESOURCE GROUP] [CIS NAME] [CIS DOMAIN] [CODE ENGINE APP URL]\n")
+    print("\t[terraform implementation]\tcis-integration [global options] --terraform [RESOURCE GROUP] [CIS NAME] [CIS DOMAIN] [CODE ENGINE APP URL]\n")
 
     print(Color.BOLD + "GLOBAL OPTIONS:" + Color.END)
     print("\t--help, -h \t\t show help\n")
 
+# handles the arguments given for both the python and terraform command options
 def handle_args():
     UserInfo = IntegrationInfo()
 
+    # handle terraform option
     UserInfo.terraforming = True
     try:
         sys.argv.index('--terraform')
     except ValueError:
         UserInfo.terraforming = False
 
+    # grab the index of the `--env` argument if it exists
     try:
         env_index = sys.argv.index('--env')
     except:
         env_index = -1
 
+    # handle case where .env file name is given
     if env_index != -1:
         if env_index == len(sys.argv) - 1:
             print("No env file found!")
@@ -109,12 +115,14 @@ def handle_args():
 
         return UserInfo.terraforming
 
+    # handle case where .env file name is not given
     args = [arg for arg in sys.argv if arg[0] != '-']
 
     if len(sys.argv) > 1 and sys.argv[1] == '-h':
         print_help()
         sys.exit(1)
 
+    # terraforming vs. not terraforming
     if UserInfo.terraforming:
         try:
             UserInfo.resource_group = args[1]
@@ -140,6 +148,7 @@ def handle_args():
             print("You did not specify a CIS Zone_ID.")
             sys.exit(1)
     
+    # common arguments
     try:
         UserInfo.cis_domain = args[3]
     except IndexError:
@@ -153,13 +162,13 @@ def handle_args():
         sys.exit(1)
 
     # temp variable github PAT while the project is private, will need to be removed once prject is public
-
     try:
         UserInfo.github_pat = args[5]
     except IndexError:
         print("You did not specify a GitHub PAT.")
         sys.exit(1)
-
+        
+    # determining API key and creating the .env file
     UserInfo.cis_api_key = getpass.getpass(prompt="Enter CIS Services API Key: ")
     UserInfo.create_envfile()
 
@@ -168,10 +177,10 @@ def handle_args():
 def main():
     terraforming = handle_args()
 
-    if terraforming:
+    if terraforming: # handle the case of using terraform
         work_creator = WorkspaceCreator()
         work_creator.create_terraform_workspace()
-    else:
+    else: # handle the case of using python
         # 1. Domain Name and DNS
         user_DNS = DNSCreator()
         user_DNS.create_records()
