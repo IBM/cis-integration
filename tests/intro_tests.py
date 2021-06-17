078c19a219652bd60d3ce13b4cc9924607bebf0b
Michael-Hilborn <Michael.Hilborn@ibm.com>
2021-06-17 13:37:03 -0500

    changed to pip3 and updated the requirements.txt. Also added error message checking

--- tests/intro_tests.py
+++ tests/intro_tests.py
@@ -2,5 +2,5 @@ import pytest
 import os
 
 def no_crn_test():
-	with pytest.raises(IndexError) as e_info:
+	with pytest.raises(IndexError, match=r"You did not specify a CIS CRN.")) as e_info:
 		os.system("cis-integration")
