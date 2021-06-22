d24841572a557ec7938e626f1003d3c9c4de1c73
Christopher Springstead <Christopher.Springstead@ibm.com>
2021-06-22 13:39:32 -0500

    working dockerization

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,14 @@ COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 COPY . .
 <<<<<<< HEAD
+<<<<<<< HEAD
 RUN python3 setup.py install
 RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
 USER appuser
 =======
 RUN pip3 install -e ./src/ -t /usr/local/bin/
 >>>>>>> 5c560c0 (update to dockerfile)
+=======
+RUN python3 setup.py install
+>>>>>>> dea0e91 (working dockerization)
 CMD [ "bash" ]
--- setup.py
+++ setup.py
@@ -8,8 +8,17 @@ setup (
     packages=["src"],
     entry_points={
         'console_scripts': [
+<<<<<<< HEAD:setup.py
             'cis-integration=src.main:main',
             'ci=src.main:main'
+=======
+<<<<<<< HEAD:src/setup.py
+            'cis-integration=main:main',
+            'ci=main:main'
+=======
+            'cis-integration=src.main:main'
+>>>>>>> d36c043 (working dockerization):setup.py
+>>>>>>> dea0e91 (working dockerization):src/setup.py
         ]
     }
 )
