b3c2d1041f8b0d43317eec46262d2cab516a0fd3
Christopher Springstead <Christopher.Springstead@ibm.com>
2021-06-24 12:22:09 -0500

    fixed install location

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 pip3 install -r requirements.txt
-python3 setup.py develop --prefix /usr/local/bin
+python3 setup.py develop --install-dir /usr/local/bin
 python3 install_check.py
