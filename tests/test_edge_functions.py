f153da7424123f1720971a9919e714d4a63bd949
Michael-Hilborn <Michael.Hilborn@ibm.com>
2021-06-25 16:35:18 -0500

    created unit tests for edge functions using mock

--- tests/test_edge_functions.py
+++ tests/test_edge_functions.py
@@ -22,8 +22,16 @@ class MockToken:
     def token(self, dummy):
         return "test-token"
 
+def edge_function():
+    return create_edge_function.EdgeFunctionCreator(
+        crn="testString",
+        app_url="test-url.com",
+        apikey="testString",
+        zone_id="testString",
+        domain="test-domain.com"
+    )
 
-def test_get_json(monkeypatch):
+def test_create_edge_action(monkeypatch):
 
     # Any arguments may be passed and mock_get() will always return our
     # mocked object, which only has the .json() method.
@@ -42,61 +50,56 @@ def test_get_json(monkeypatch):
     result = test_edge_function.create_edge_function_action()
     assert result.json()["success"] == True
 
-def edge_function():
-    return create_edge_function.EdgeFunctionCreator(
-        crn="testString",
-        app_url="test-url.com",
-        apikey="testString",
-        zone_id="testString",
-        domain="test-domain.com"
-    )
+def test_create_edge_trigger(monkeypatch):
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object, which only has the .json() method.
+    def mock_put(*args, **kwargs):
+        return MockResponse()
 
-def test_create_edge_action():
-    #test the creation of edge function action
-    test_edge_function = edge_function()
-    result = test_edge_function.create_edge_function_action()
-    print("Test Create Edge Action 1")
-    print(result)
-    pytest.assertEqual(result.status_code, 200)
+    def mock_token(*args, **kwargs):
+        return MockToken()
 
-def test_create_edge_trigger():
-    #test the creation of edge function trigger
+    # apply the monkeypatch for requests.get to mock_get
+    monkeypatch.setattr(requests, "request", mock_put)
+    monkeypatch.setattr(create_edge_function.EdgeFunctionCreator, "request_token", mock_token().token)
+
+    # app.get_json, which contains requests.get, uses the monkeypatch
     test_edge_function = edge_function()
     result = test_edge_function.create_edge_function_trigger()
-    print("Test Create Edge Trigger 1.1")
-    print(result)
-    assert result.status_code == 200
+    assert result.json()["success"] == True
 
-    #test the creation of an already existing edge function trigger
-    result = test_edge_function.create_edge_function_trigger()
-    print("Test Create Edge Trigger 1.2")
-    print(result)
-    assert result.status_code == 409
+def test_create_edge_www_trigger(monkeypatch):
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object, which only has the .json() method.
+    def mock_put(*args, **kwargs):
+        return MockResponse()
 
-def test_create_edge_www_trigger():
-    #test the creation of edge function trigger
+    def mock_token(*args, **kwargs):
+        return MockToken()
+
+    # apply the monkeypatch for requests.get to mock_get
+    monkeypatch.setattr(requests, "request", mock_put)
+    monkeypatch.setattr(create_edge_function.EdgeFunctionCreator, "request_token", mock_token().token)
+
+    # app.get_json, which contains requests.get, uses the monkeypatch
     test_edge_function = edge_function()
     result = test_edge_function.create_edge_function_www_trigger()
-    print("Test Create Edge Trigger 2.1")
-    print(result)
-    assert result.status_code == 200
+    assert result.json()["success"] == True
 
-    #test the creation of an already existing edge function trigger
-    result = test_edge_function.create_edge_function_www_trigger()
-    print("Test Create Edge Trigger 2.2")
-    print(result)
-    assert result.status_code == 409
+def test_create_edge_trigger(monkeypatch):
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object, which only has the .json() method.
+    def mock_put(*args, **kwargs):
+        return MockResponse()
 
-def test_create_edge_trigger():
-    #test the creation of edge function trigger
+    def mock_token(*args, **kwargs):
+        return MockToken()
+
+    # apply the monkeypatch for requests.get to mock_get
+    monkeypatch.setattr(requests, "request", mock_put)
+    monkeypatch.setattr(create_edge_function.EdgeFunctionCreator, "request_token", mock_token().token)
+
+    # app.get_json, which contains requests.get, uses the monkeypatch
     test_edge_function = edge_function()
     result = test_edge_function.create_edge_function_wild_card_trigger()
-    print("Test Create Edge Trigger 3.1")
-    print(result)
-    assert result.status_code == 200
-
-    #test the creation of an already existing edge function trigger
-    result = test_edge_function.create_edge_function_wild_card_trigger()
-    print("Test Create Edge Trigger 3.2")
-    print(result)
-    assert result.status_code == 409
+    assert result.json()["success"] == True
