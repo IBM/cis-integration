6d07be41b4f4f890b25497d0fac426698d331cdd
Akul Joshi <joshi.akul@ibm.com>
2021-06-25 17:01:31 -0500

    created a mock model and test for the DNS class

--- tests/test_dns_creator.py
+++ tests/test_dns_creator.py
@@ -141,12 +141,48 @@ def test_both_exist(monkeypatch):
     assert www_record.result["result"]["name"] == "www"
     assert www_record.result["result"]["content"] == "test-url.com"
 
-class MockDNSRecords:
-    pass
+# custom class to be a mock DNS Records object
+# will override the DnsRecordsV1 object in dns_creator.py
+class MockDNSRecordsV1Neither:
+
+    # mock set_service_url() serves as a useless mock method
+    def set_service_url(self, url):
+        pass
+
+    # mock list_all_dns_records() creates a fake list of dns records
+    def list_all_dns_records(self):
+        return DetailedResponse(response={"result": []})
+
+    # create_dns_records() creates a fake DNS record for testing
+    def create_dns_record(self, type, name, content, proxied):
+        return DetailedResponse(response={"result": { "id": "testId", "name": name, "type": type, "content": content, "proxied": True}})
+
+#################################################
+##### TO DO
+##### - neither records exist
+##### - both records exist
+##### - one record exists
+#################################################
 
 def test_neither_exist(monkeypatch):
-    monkeypatch.setattr()
-    pass
+
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object
+    def mock_get(*args, **kwargs):
+        return MockDNSRecordsV1Neither()
+
+    monkeypatch.setattr(DnsRecordsV1, "new_instance", mock_get)
+    creator = dns_creator()
+    root_record, www_record = creator.create_records()
+
+    assert root_record.result["result"]["type"] == "CNAME"
+    assert root_record.result["result"]["name"] == "@"
+    assert root_record.result["result"]["content"] == "test-url.com"
+
+    assert www_record.result["result"]["type"] == "CNAME"
+    assert www_record.result["result"]["name"] == "www"
+    assert root_record.result["result"]["content"] == "test-url.com"
+
 
 def dns_creator():
     return DNSCreator(
