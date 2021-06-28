37c78b684edee5527eaee26883ac8b22d9078806
Akul Joshi <joshi.akul@ibm.com>
2021-06-27 22:10:06 -0500

    wrote the tests for the DNS creator

--- tests/test_dns_creator.py
+++ tests/test_dns_creator.py
@@ -16,19 +16,77 @@ class MockDNSRecordsV1Neither:
     def set_service_url(self, url):
         pass
 
-    # mock list_all_dns_records() creates a fake list of dns records
+    # mock list_all_dns_records() creates a fake list of DNS records
     def list_all_dns_records(self):
         return DetailedResponse(response={"result": []})
 
-    # create_dns_records() creates a fake DNS record for testing
+    # mock create_dns_records() creates a fake DNS record for testing
     def create_dns_record(self, type, name, content, proxied):
-        return DetailedResponse(response={"result": { "id": "testId", "name": name, "type": type, "content": content, "proxied": True}})
+        return DetailedResponse(response={"result": { "id": "testId", "name": name, "type": type, "content": content, "proxied": proxied}})
+
+# custom class to be a mock DNS Records object
+# will override the DnsRecordsV1 object in dns_creator.py
+class MockDNSRecordsV1Root:
+    def __init__(self):
+        self.records_list = []
+        root_record = { "id": "testId", "created_on": "testDate", "modified_on": "testDate", "name": "test.com", "type": "CNAME", "zone_name": "test.com" }
+        self.records_list.append(root_record)
+
+    # mock set_service_url() serves as a useless mock method
+    def set_service_url(self, url):
+        pass
+
+    # mock list_all_dns_records() creates a fake list of DNS records
+    def list_all_dns_records(self):
+        return DetailedResponse(response={"result": self.records_list})
+
+    # mock create_dns_records() creates a fake DNS record for testing
+    def create_dns_record(self, type, name, content, proxied):
+        return DetailedResponse(response={"result": { "id": "testId", "name": name, "type": type, "content": content, "proxied": proxied}})
+    
+    # mock update_dns_records() updates a fake DNS record for testing
+    def update_dns_record(self, dnsrecord_identifier, type, name, content, proxied):
+        for item in self.records_list:
+            if item['id'] == dnsrecord_identifier:
+                item['type'] = type
+                item['name'] = name
+                item['content'] = content
+                item['proxied'] = proxied
+
+                return DetailedResponse(response={"result": item})
+
+# custom class to be a mock DNS Records object
+# will override the DnsRecordsV1 object in dns_creator.py
+class MockDNSRecordsV1Both:
+    def __init__(self):
+        self.records_list = []
+        root_record = { "id": "testId", "created_on": "testDate", "modified_on": "testDate", "name": "test.com", "type": "CNAME", "zone_name": "test.com" }
+        www_record = { "id": "testId2", "created_on": "testDate", "modified_on": "testDate", "name": "www.test.com", "type": "CNAME", "zone_name": "test.com" }
+        self.records_list.append(root_record)
+        self.records_list.append(www_record)
+
+    # mock set_service_url() serves as a useless mock method
+    def set_service_url(self, url):
+        pass
+
+    # mock list_all_dns_records() creates a fake list of DNS records
+    def list_all_dns_records(self):
+        return DetailedResponse(response={"result": self.records_list})
+
+    # mock update_dns_records() updates a fake DNS record for testing
+    def update_dns_record(self, dnsrecord_identifier, type, name, content, proxied):
+        for item in self.records_list:
+            if item['id'] == dnsrecord_identifier:
+                item['type'] = type
+                item['name'] = name
+                item['content'] = content
+                item['proxied'] = proxied
+
+                return DetailedResponse(response={"result": item})
 
 #################################################
 ##### TO DO
-##### - neither records exist
 ##### - both records exist
-##### - one record exists
 #################################################
 
 def test_neither_exist(monkeypatch):
@@ -50,6 +108,43 @@ def test_neither_exist(monkeypatch):
     assert www_record.result["result"]["name"] == "www"
     assert root_record.result["result"]["content"] == "test-url.com"
 
+def test_one_exist(monkeypatch):
+
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object
+    def mock_get(*args, **kwargs):
+        return MockDNSRecordsV1Root()
+    
+    monkeypatch.setattr(DnsRecordsV1, "new_instance", mock_get)
+    creator = dns_creator()
+    root_record, www_record = creator.create_records()
+
+    assert root_record.result["result"]["type"] == "CNAME"
+    assert root_record.result["result"]["name"] == "@"
+    assert root_record.result["result"]["content"] == "test-url.com"
+    
+    assert www_record.result["result"]["type"] == "CNAME"
+    assert www_record.result["result"]["name"] == "www"
+    assert www_record.result["result"]["content"] == "test-url.com"
+
+def test_both_exist(monkeypatch):
+
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object
+    def mock_get(*args, **kwargs):
+        return MockDNSRecordsV1Both()
+    
+    monkeypatch.setattr(DnsRecordsV1, "new_instance", mock_get)
+    creator = dns_creator()
+    root_record, www_record = creator.create_records()
+
+    assert root_record.result["result"]["type"] == "CNAME"
+    assert root_record.result["result"]["name"] == "@"
+    assert root_record.result["result"]["content"] == "test-url.com"
+    
+    assert www_record.result["result"]["type"] == "CNAME"
+    assert www_record.result["result"]["name"] == "www"
+    assert www_record.result["result"]["content"] == "test-url.com"
 
 def dns_creator():
     return DNSCreator(
@@ -59,22 +154,3 @@ def dns_creator():
         app_url="test-url.com"
     )
 
-def test_create_root_record():
-    dns = dns_creator()
-    (response, new_record) = dns.create_root_record()
-    assert response.status_code == 200
-    assert new_record == True
-
-    (response, new_record) = dns.create_root_record()
-    assert response.status_code == 200
-    assert new_record == False
-
-def test_create_www_record():
-    dns = dns_creator()
-    (response, new_record) = dns.create_www_record()
-    assert response.status_code == 200
-    assert new_record == True
-
-    (response, new_record) = dns.create_www_record()
-    assert response.status_code == 200
-    assert new_record == False
