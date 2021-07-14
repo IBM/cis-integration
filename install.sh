2d1cf19197d34e4614b6cf045570ea1a7685e55a
Christopher Springstead <chrisspringstead15@gmail.com>
2021-07-14 14:31:28 -0500

    updates to install and added try block to catch any exceptions during uninstall

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/usr/local/bin
 pip3 install -r requirements.txt
-python3 setup.py develop --install-dir /usr/local/bin
+pip3 install -e . --target /usr/local/bin
 python3 install_check.py
