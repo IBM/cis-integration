9b5089b3d6650c5604e536c9829a4d34de649768
akulio4668 <joshi.akul@gmail.com>
2021-07-16 14:30:38 -0500

    added code to create secret using the certificate CRN returned from the cert manager

--- src/certcreate_iks.py
+++ src/certcreate_iks.py
@@ -11,13 +11,15 @@ class SecretCertificateCreator:
         self.cert_name = cert_name
         self.token = token
 
+    # Creates a secret in the IKS cluster using the CRN returned by check_certificate()
     def create_secret(self):
+        cert_crn = self.check_certificate()
         cert_url = "https://containers.cloud.ibm.com/global/ingress/v2/secret/createSecret"
         
         # Creating the data required for the request
         cert_data = json.dumps({
             "cluster": self.cluster_id,
-            "crn": self.cert_manager_crn,
+            "crn": cert_crn,
             "name": self.cert_name,
             "namespace": "default",
             "persistence": True
@@ -39,6 +41,7 @@ class SecretCertificateCreator:
 
         return cert_response
 
+    # Creates a certificate (if necessary) and returns a CRN
     def check_certificate(self):
         url_cert_man_crn = self.URLify(self.cert_manager_crn)
         try:
@@ -53,7 +56,10 @@ class SecretCertificateCreator:
             "Authorization": 'Bearer ' + self.token
         }
 
+        # Gets all certificates previously present in the certificate manager
         cert_check_response = requests.request("GET", url=cert_check_url, headers=cert_check_headers)
+        
+        # If a valid certificate exists, it returns the CRN of that certificate
         for cert in cert_check_response.json()["certificates"]:
             if self.cis_domain in cert["domains"] and ("*." + self.cis_domain) in cert["domains"]:
                 print("Certificate with domain already exists in certificate manager")
@@ -66,7 +72,8 @@ class SecretCertificateCreator:
         cert_create_data = json.dumps({
             "name": self.cert_name,
             "domains": [self.cis_domain, "*."+self.cis_domain],
-            "dns_provider_instance_crn": self.cis_crn
+            "dns_provider_instance_crn": self.cis_crn,
+            "auto_renew_enabled": True
         })
 
         cert_create_headers = {
@@ -74,12 +81,14 @@ class SecretCertificateCreator:
             "authorization": 'Bearer ' + self.token
         }
 
+        # Orders a new certificate through the certificate manager
         cert_create_response = requests.request("POST", url=cert_create_url, headers=cert_create_headers, data=cert_create_data)
-       
+
+        # Returns the CRN of the new certificate
         print(Color.GREEN+"SUCESS: Ordered a certificate for the certificate manager!"+Color.END)
         return cert_create_response.json()["_id"]
         
-
+    # Converts the certificate manager CRN into a URL-encoded CRN
     def URLify(self, replacement_str):
         new_string = replacement_str.replace(":", "%3A")
         return new_string.replace("/", "%2F")
