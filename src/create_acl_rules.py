0ec47ca35509e4d2bff1ad052955c00fb1e241c3
Christopher Springstead <chrisspringstead15@gmail.com>
2021-07-19 14:12:43 -0500

    refactor

--- src/create_acl_rules.py
+++ src/create_acl_rules.py
@@ -11,15 +11,15 @@ class AclRuleCreator:
         self.vpc_name = vpc_name
         self.vpc_id = ''
 
-    def create_acl(self):
-        authenticator = IAMAuthenticator(self.api_key)
-
+    def create_resource_group_model(self, authenticator):
         resource_service = ResourceManagerV2(authenticator=authenticator)
-        resource = resource_service.list_resource_groups(name=self.resource_group, include_deleted=false).get_result()
+        resource = resource_service.list_resource_groups(name=self.resource_group, include_deleted=False).get_result()
         self.resource_group_id = resource["resources"][0]["id"]
         resource_group_identity_model = {}
         resource_group_identity_model['id'] = self.resource_group_id
-        
+        return resource_group_identity_model
+
+    def create_vpc_model(self, authenticator):
         vpc_serivce = VpcV1(authenticator=authenticator)
         vpc_list = vpc_serivce.list_vpcs(resource_group_id=self.resource_group_id).get_result()
         for vpc in vpc_list["vpcs"]:
@@ -27,6 +27,19 @@ class AclRuleCreator:
                 self.vpc_id = vpc["id"]
         vpc_identity_model = {}
         vpc_identity_model["id"] = self.vpc_id
+        return vpc_identity_model
+
+    def create_acl_rules(self):
+
+
+    def create_network_acl(self):
+        authenticator = IAMAuthenticator(self.api_key)
+
+        resource_group_identity_model = self.create_resource_group_model(authenticator)
+        
+        vpc_identity_model = self.create_vpc_model(authenticator)
        
         network_acl_prototype_model = {}
         network_acl_prototype_model['name'] = "cis-integration_default"
+        network_acl_prototype_model["resource_group"] = resource_group_identity_model
+        network_acl_prototype_model["vpc"] = vpc_identity_model
