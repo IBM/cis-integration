2495838ec142b80015ffa8830a4fc54659126053
Christopher Springstead <chrisspringstead15@gmail.com>
2021-07-20 14:35:58 -0500

    created all needed rules, testing now

--- src/create_acl_rules.py
+++ src/create_acl_rules.py
@@ -27,7 +27,7 @@ class AclRuleCreator:
                 self.vpc_id = vpc["id"]
         vpc_identity_model = {}
         vpc_identity_model["id"] = self.vpc_id
-        return vpc_identity_model
+        return vpc_identity_model, vpc_serivce
 
     def create_acl_rules(self):
         network_acl_rules = [
@@ -37,6 +37,7 @@ class AclRuleCreator:
                 "source" : "161.26.0.0/16",
                 "destination" : "0.0.0.0/0",
                 "direction" : "inbound",
+                "protocol" : "all",
             },
             {
                 "name" : "iks-nodes-to-master-inbound",
@@ -44,8 +45,213 @@ class AclRuleCreator:
                 "source" : "166.8.0.0/14",
                 "destination": "0.0.0.0/0",
                 "direction" : "inbound",
-            }
+                "protocol" : "all",
+            },
+            {
+                "name" : "cf-ip-1",
+                "action" : "allow",
+                "source" : "103.21.244.0/22",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-2",
+                "action" : "allow",
+                "source" : "173.245.48.0/20",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-3",
+                "action" : "allow",
+                "source" : "103.22.200.0/22",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-4",
+                "action" : "allow",
+                "source" : "103.31.4.0/22",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-5",
+                "action" : "allow",
+                "source" : "141.101.64.0/18",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-6",
+                "action" : "allow",
+                "source" : "108.162.192.0/18",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-7",
+                "action" : "allow",
+                "source" : "190.93.240.0/20",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-8",
+                "action" : "allow",
+                "source" : "188.114.96.0/20",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-9",
+                "action" : "allow",
+                "source" : "197.234.240.0/22",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-10",
+                "action" : "allow",
+                "source" : "198.41.128.0/17",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-11",
+                "action" : "allow",
+                "source" : "162.158.0.0/15",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-12",
+                "action" : "allow",
+                "source" : "104.16.0.0/12",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-13",
+                "action" : "allow",
+                "source" : "172.64.0.0/13",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "cf-ip-14",
+                "action" : "allow",
+                "source" : "131.0.72.0/22",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "tcp",
+                "source_port_min" : 1,
+                "source_port_max" : 65535,
+                "destination_port_min" : 443,
+                "destination_port_max" : 443
+            },
+            {
+                "name" : "worker-1",
+                "action" : "allow",
+                "source" : "10.10.30.0/24",
+                "destination" : "0.0.0.0/0",
+            },
+            {
+                "name" : "worker-2",
+                "action" : "allow",
+                "source" : "10.10.20.0/24",
+                "destination" : "0.0.0.0/0",
+            },
+            {
+                "name" : "worker-3",
+                "action" : "allow",
+                "source" : "10.10.10.0/24",
+                "destination" : "0.0.0.0/0",
+            },
+            {
+                "name" : "allow-all-outbound",
+                "action" : "allow",
+                "source" : "0.0.0.0/0",
+                "destination" : "0.0.0.0/0",
+                "direction" : "outbound",
+                "protocol" : "all",
+            },
+            {
+                "name" : "deny-all-inbound",
+                "action" : "deny",
+                "source" : "0.0.0.0/0",
+                "destination" : "0.0.0.0/0",
+                "direction" : "inbound",
+                "protocol" : "all",
+            },               
         ]
+
+        return network_acl_rules
 
 
 
@@ -55,9 +261,13 @@ class AclRuleCreator:
 
         resource_group_identity_model = self.create_resource_group_model(authenticator)
         
-        vpc_identity_model = self.create_vpc_model(authenticator)
+        vpc_identity_model, vpc_service = self.create_vpc_model(authenticator)
        
         network_acl_prototype_model = {}
         network_acl_prototype_model['name'] = "cis-integration_default"
         network_acl_prototype_model["resource_group"] = resource_group_identity_model
         network_acl_prototype_model["vpc"] = vpc_identity_model
+        network_acl_prototype_model["rules"] = self.create_acl_rules()
+
+        response = vpc_service.create_network_acl(network_acl_prototype_model=network_acl_prototype_model)
+        print(response)
