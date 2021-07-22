9b09a82105cb05ed9668bc40b25f7c0cebfa3c49
akulio4668 <joshi.akul@gmail.com>
2021-07-22 10:08:56 -0500

    added the initial workings of a Terraform module for IKS

--- src/root_terraform/main.tf
+++ src/root_terraform/main.tf
@@ -1,9 +1,19 @@
-module ibm_cis_integration {
+module ibm_cis_ce_integration {
     source = "../ce/terraform"
+    ibmcloud_api_key = var.ibmcloud_api_key
     cis_name = var.cis_name
     resource_group = var.resource_group
     app_url = var.app_url
     cis_domain = var.cis_domain
-    ibmcloud_api_key = var.ibmcloud_api_key
     pool_name = var.pool_name
+}
+
+module ibm_cis_iks_integration {
+    source = "../iks/terraform"
+    ibmcloud_api_key = var.ibmcloud_api_key
+    cis_name = var.cis_name
+    resource_group = var.resource_group
+    cis_domain = var.cis_domain
+    cluster_id = var.cluster_id
+    token = var.token
 }
